661580f378e1f27e41b4331bb36cc6e449bd92db
Ilona Lapka <ilona2000123@wp.pl>
2022-06-27 14:18:14 +0200

    Initial

---
+++ Aplikacja1/Person.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja1
+{
+    public class Person
+    {
+        [JsonProperty(PropertyName = "imię")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "nazwisko")]
+        public string Surname { get; set; }
+
+        [JsonProperty(PropertyName = "zawód")]
+        public string Profession { get; set; }
+
+        [JsonProperty(PropertyName = "wiek")]
+        public int Age { get; set; }
+
+        [JsonProperty(PropertyName = "miejsce-urodzenia")]
+        public string PlaceOfBirth { get; set; }
+    }
+}
---
+++ Aplikacja1/Program.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Aplikacja1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            do
+            {
+                Console.Write("Podaj sciezke do pliku: ");
+                string path = Console.ReadLine();
+
+                List<Person> persons = new List<Person>();
+
+                try
+                {
+                    using StreamReader r = new StreamReader(path);
+                    string json = r.ReadToEnd();
+                    if ((IsValidJson(json)))
+                    {
+
+                        persons = JsonConvert.DeserializeObject<List<Person>>(json);
+
+                        MakeLine();
+                        Console.WriteLine("|  IMIĘ  |  NAZWISKO  |  ZAWÓD  |  WIEK  |  MIEJSCE-URODZENIA  |");
+                        MakeLine();
+
+                        foreach (var person in persons)
+                        {
+                            Console.WriteLine("|  {0}  |  {1}  |  {2}  |  {3}  |  {4}  |", person.Name, person.Surname,
+                                person.Profession, person.Age, person.PlaceOfBirth);
+                            MakeLine();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+                Console.WriteLine("Czy wczytać kolejny plik? t/n");
+                answer = Console.ReadLine();
+            } while (answer == "t");
+        }
+
+        static bool IsValidJson(string strInput)
+        {
+            if (string.IsNullOrWhiteSpace(strInput)) { return false; }
+            strInput = strInput.Trim();
+            if ((strInput.StartsWith("{") && strInput.EndsWith("}")) || //For object
+                (strInput.StartsWith("[") && strInput.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(strInput);
+                    foreach (var item in obj)
+                    {
+                        foreach (var value in item.Values())
+                        {
+                            if(value.Type == JTokenType.Array || value.Type == JTokenType.Object)
+                            {
+                                Console.WriteLine("Plik nie powinien zawierać zagnieżdżeń obiektów, " +
+                                    "ani tablic w danej właściwości");
+                                return false;
+                            }
+                        }
+                        
+                    }
+                    return true;
+                }
+                catch (JsonReaderException jex)
+                {
+                    //Exception in parsing json
+                    Console.WriteLine(jex.Message);
+                    return false;
+                }
+                catch (Exception ex) //some other exception
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nieprawidłowy format.");
+                return false;
+            }
+        }
+
+        static void MakeLine()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine();
+        }
+    }
+}
